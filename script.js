b9a1c04dbdad0bfbe03e083f9161eb206b3234ad
VadimUa <vadia62beck@gmail.com>
2019-05-06 16:10:16 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
+let exitcase =  setInterval(move_image_right, 1000);
 var image = document.querySelector('img.tank');
 image.style.setProperty('position', 'absolute');
-image.style.setProperty('left', '100px');
+image.style.setProperty('left', '5px');
+image.style.setProperty('right', '5px');
 
-    function move_image(){
+    function move_image_right(){
     let left = image.style.left;
     left = parseInt(left);
     left +=25; 
     image.style.left = left + 'px';
     }
     
-let exitcase =  setInterval(move_image, 1000);
-if( left > screen.width){
-    exitcase = clearInterval(exitcase);
+move_image_right();
+
+if( image.onclick() ){
+   function move_image_left(){
+    let right = image.style.right;
+    right = parseInt(right);
+    right +=25; 
+    image.style.right = right + 'px'; 
+   }
 }
+
